3b10033bdf7dbc5c6f3d0b917a93a608a80c8e11
ChairGamertag87 <76748554+ChairGamertag87@users.noreply.github.com>
2026-01-06 16:29:14 +0100

    ✨Add antivirus and test preparation scripts

---
+++ antivirus.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ==============================================================================
+# Nom du script : antivirus.sh
+# Description   : Gestion de fichiers corrompus selon un critère défini.
+# Auteur        : Herrard Clément, Navarro Thomas
+# Date          : 06/01/2026
+# Paramètres    : $1 = chemin du fichier contenant le critère (ex: .sha)
+# Codes retour  : 0 (Succès), 1 (Erreur argument), 2 (Fichier introuvable)
+# ==============================================================================
---
+++ prepare_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ==============================================================================
+# Nom du script : prepare_test.sh
+# Description   : Installation des fichiers pour les tests de antivirus.sh
+# Auteur        : Herrard Clément, Navarro Thomas
+# Date          : 06/01/2026
+# Paramètres    : Aucun
+# Codes retour  : 0 (Succès), 1 (Erreur Code)
+# ==============================================================================
